5427e0c802e838119a13328a2a6d6e7719087397
Benjamin Nørgaard <benjamin@webtoad.dk>
2013-09-11 13:16:50 +0200

    Adds a dropdown which selects a google account from the phone

--- src/dk/au/cs/skatespots/LoginActivity.java
+++ src/dk/au/cs/skatespots/LoginActivity.java
@@ -1,10 +1,16 @@
 package dk.au.cs.skatespots;
 
+import java.util.ArrayList;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class LoginActivity extends Activity {
 
@@ -12,6 +18,7 @@ public class LoginActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		setUpAccountDropDown();
 	}
 
 	@Override
@@ -19,6 +26,25 @@ public class LoginActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
+	}
+	
+	private void setUpAccountDropDown() {
+		
+		AccountManager am = AccountManager.get(this);
+		Account[] accounts = am.getAccountsByType("com.google");
+		ArrayList<String> accountStrings = new ArrayList<String>();
+		for (Account a : accounts) {
+			accountStrings.add(a.name);
+		}
+		
+		if (accountStrings.isEmpty()) {
+			// Lav en dialog som siger at man skal logge ind på en google konto
+		} else {
+			Spinner user_name = (Spinner) findViewById(R.id.user_name);
+			ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, accountStrings);
+			dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			user_name.setAdapter(dataAdapter);
+		}
 	}
 	
 	public void login(View view) {
