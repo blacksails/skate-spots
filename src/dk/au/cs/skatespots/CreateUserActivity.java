e5e724831407d489b520cc24ca0df00f877bcf3a
marcc92 <marc_madsen@hotmail.com>
2013-09-18 12:11:49 +0200

    Missed a few files last commit

---
+++ src/dk/au/cs/skatespots/CreateUserActivity.java
@@ -0,0 +1,61 @@
+package dk.au.cs.skatespots;
+
+import java.util.ArrayList;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class CreateUserActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_create_user);
+		setUpAccountDropDown();		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+	
+	private void setUpAccountDropDown() {
+		
+		AccountManager am = AccountManager.get(this);
+		Account[] accounts = am.getAccountsByType("com.google");
+		ArrayList<String> accountStrings = new ArrayList<String>();
+		for (Account a : accounts) {
+			accountStrings.add(a.name);
+		}
+		
+		if (accountStrings.isEmpty()) {
+			// Lav en dialog som siger at man skal logge ind på en google konto
+		} else {
+			Spinner user_name = (Spinner) findViewById(R.id.user_name);
+			ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, accountStrings);
+			dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			user_name.setAdapter(dataAdapter);
+		}
+	}
+	
+	public void checkEligibilityLogin(View view){
+		//if(Login doesn't exist, then)
+		mainActivity();
+		//else if login does exist
+	}
+	
+	private void mainActivity() {
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+	}
+
+}
