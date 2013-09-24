fac769a06bf0c336753a0da6f7fe50455e37d942
marcc92 <marc_madsen@hotmail.com>
2013-09-24 11:29:06 +0200

    Added LocationClient service and Toast to reflect

--- src/dk/au/cs/skatespots/MainActivity.java
+++ src/dk/au/cs/skatespots/MainActivity.java
@@ -2,15 +2,25 @@ package dk.au.cs.skatespots;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 
-public class MainActivity extends Activity {
-	public static Context c;
+public class MainActivity extends Activity implements ConnectionCallbacks, 
+															  OnConnectionFailedListener 
+															  //OnAddGeofencesResultListener 
+															  {
+	LocationClient locationClient;
+	private GoogleMap map;
+	private Location location;
 	
 
 	@Override
@@ -18,7 +28,8 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		setUpMapIfNeeded();
-		
+		locationClient = new LocationClient(this, this, this);
+		locationClient.connect();
 		
 	}
 
@@ -35,10 +46,38 @@ public class MainActivity extends Activity {
 	        // Check if we were successful in obtaining the map.
 	        if (map != null) {
 	        	map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-	        	map.setMyLocationEnabled(true);
+	        	//map.setMyLocationEnabled(true);
+	        	//Test
 	        }
 	    }
 	}
 
-	private GoogleMap map;
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		location = locationClient.getLastLocation();
+		String currentCoordinates = location.toString();
+		
+		
+		//Adds a toast that pops up with our current coordinates once connected.
+		Context context = getApplicationContext();
+		CharSequence text = currentCoordinates;
+		int duration = Toast.LENGTH_LONG;
+
+		Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
+		
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
