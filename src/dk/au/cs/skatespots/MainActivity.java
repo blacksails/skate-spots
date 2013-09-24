2c800a9ee7a186ab4f5a3439255bf2aa45b8dcdb
marcc92 <marc_madsen@hotmail.com>
2013-09-24 13:34:09 +0200

    Added LocationClient (Coordinates) and Automatic Zoom to current position

--- src/dk/au/cs/skatespots/MainActivity.java
+++ src/dk/au/cs/skatespots/MainActivity.java
@@ -20,11 +20,11 @@ import com.google.android.gms.maps.model.LatLng;
 public class MainActivity extends Activity implements ConnectionCallbacks, 
 													  OnConnectionFailedListener 
 													  //OnAddGeofencesResultListener 
-															  {
+{
 	LocationClient locationClient;
 	private GoogleMap map;
 	private Location location;	
-	
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +33,7 @@ public class MainActivity extends Activity implements ConnectionCallbacks,
 		setUpMapIfNeeded();
 		locationClient = new LocationClient(this, this, this);
 		locationClient.connect();
-		
+
 	}
 
 	@Override
@@ -42,53 +42,53 @@ public class MainActivity extends Activity implements ConnectionCallbacks,
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	private void setUpMapIfNeeded() {
-	    if (map == null) {
-	        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-	        // Check if we were successful in obtaining the map.
-	        if (map != null) {
-	        	map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-	        
-	        	 //map.animateCamera(CameraUpdateFactory.zoomIn()); //Zooms in
-	        	 //map.setMyLocationEnabled(true);
-	        }
-	    }
+		if (map == null) {
+			map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+			// Check if we were successful in obtaining the map.
+			if (map != null) {
+				map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+				//map.animateCamera(CameraUpdateFactory.zoomIn()); //Zooms in
+				//map.setMyLocationEnabled(true);
+			}
+		}
 	}
 
 
 	@Override
 	public void onConnected(Bundle arg0) {
 		location = locationClient.getLastLocation();
-		
+
 		//Zooms in on our current position
 		LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-    	CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
-    	map.animateCamera(cameraUpdate);
-		
+		CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
+		map.animateCamera(cameraUpdate);
+
 		//Adds a toast that pops up with our current coordinates once connected.
-    	String currentCoordinates = location.toString();
+		String currentCoordinates = location.toString();
 		Context context = getApplicationContext();
 		CharSequence text = currentCoordinates;
 		int duration = Toast.LENGTH_LONG;
 
 		Toast toast = Toast.makeText(context, text, duration);
 		toast.show();
-		
-		
-		
-		
+
+
+
+
 	}
 
 	@Override
 	public void onDisconnected() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onConnectionFailed(ConnectionResult arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
