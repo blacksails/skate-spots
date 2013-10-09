77aadc6125eb9951e4c12b14128d9a66a090b267
marcc92 <marc_madsen@hotmail.com>
2013-10-09 16:06:12 +0200

    Added responseHandler

--- src/dk/au/cs/skatespots/NewSkateSpot.java
+++ src/dk/au/cs/skatespots/NewSkateSpot.java
@@ -3,6 +3,7 @@ package dk.au.cs.skatespots;
 import java.util.ArrayList;
 
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
@@ -20,7 +21,7 @@ import android.widget.Toast;
 
 public class NewSkateSpot extends Activity {
 	SkateSpots app;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -41,26 +42,26 @@ public class NewSkateSpot extends Activity {
 		skateSpotTypes.add("Street");
 		skateSpotTypes.add("Park");
 		skateSpotTypes.add("Indoor");
-		
+
 		Spinner spinner = (Spinner) findViewById(R.id.new_skatespot_spinner);
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, skateSpotTypes);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinner.setAdapter(adapter);		
 	}
-	
-	public void createNewSkateSpot(){
+
+	public void createNewSkateSpot(View view){
 		//Finds the name of the SkateSpot
 		EditText skatespot_name = (EditText) findViewById(R.id.new_skatespot_name);
 		String name = skatespot_name.getText().toString();
-		
+
 		//Finds the description of the SkateSpot
 		EditText skatespot_description = (EditText) findViewById(R.id.new_skatespot_description);
 		String description = skatespot_description.getText().toString();
-		
+
 		//Finds the chosen type from the spinner
 		Spinner spinner = (Spinner) findViewById(R.id.new_skatespot_spinner);
 		String type = spinner.getSelectedItem().toString();
-		
+
 		//Finds the current location
 		Location loc = app.getLocation();
 		double latitude = loc.getLatitude();
@@ -75,22 +76,40 @@ public class NewSkateSpot extends Activity {
 		obj.add("latitude", new JsonPrimitive(latitude));
 		obj.add("longitude", new JsonPrimitive(longitude));
 
-		AsyncHttpResponseHandler responseHandler = new AsyncHttpResponseHandler();	
+		AsyncHttpResponseHandler responseHandler = new AsyncHttpResponseHandler() {
+			@Override
+			public void onSuccess(String response) {				
+				mainActivity();
+
+				//Display to the user that he created a new Skatespot
+				Context context = getApplicationContext();
+				CharSequence text = "You have created a new Skatespot!";
+				int duration = Toast.LENGTH_LONG;
+
+				Toast toast = Toast.makeText(context, text, duration);
+				toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL, 0, 0);
+				toast.show();
+
+			}
+			@Override
+			public void onFailure(Throwable error, String content) {
+
+				//Makes a toast to display failure
+				Context context = getApplicationContext();
+				CharSequence text = "Failed to create a new Skatespot";
+				int duration = Toast.LENGTH_LONG;
+
+				Toast toast = Toast.makeText(context, text, duration);
+				toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL, 0, 0);
+				toast.show();
+			}
+		};	
 		SkateSpotsHttpClient.post(getApplicationContext(), obj, responseHandler);
 	}
-	
-	public void createNewSkatespot(View view){
-		//Go to mainactivity
+
+	private void mainActivity() {
 		Intent intent = new Intent(this, MainActivity.class);
 		startActivity(intent);
-		
-		//Display to the user that he created a new Skatespot
-		Context context = getApplicationContext();
-		CharSequence text = "You have created a new Skatespot!";
-		int duration = Toast.LENGTH_LONG;
-
-		Toast toast = Toast.makeText(context, text, duration);
-		toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL, 0, 0);
-		toast.show();
 	}
+
 }
